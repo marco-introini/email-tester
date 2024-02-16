62006c7bfad26b31c941a650664c1625246761a4
Marco "Mint" Introini <mint.dev@pm.me>
2024-02-16 15:40:11 +0100

    Add new layouts, components, views, and configurations
    This update introduces several new layouts, components, and view templates to enhance the application's UI. It also includes the addition of new configuration files to manage these features more effectively. Changes cover various platform specifications, from mobile to desktop interfaces.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Verifica email</title>
+</head>
+<body>
+
+
+<div class="m-5">
+
+    <form action="{{route('invia-email')}}" method="post">
+        @csrf
+        <x-textarea
+            label="Html Email"
+            name="testo"
+            rows="10"
+            inline/>
+
+        <x-button
+            label="Invia email"
+            type="submit"
+        />
+
+    </form>
+</div>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::post('invia-email', function (){
+    $testoHtml = request()->post('testo');
+
+    \Illuminate\Support\Facades\Mail::html($testoHtml, function ($message){
+        $message->from('noreply@example.com', 'Example');
+        $message->to('john@example.com');
+        $message->subject('Esempio Invio email');
+    });
+
+    echo "Mail inviata";
+})->name('invia-email');
