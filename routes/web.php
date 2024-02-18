bcc7c38d9481bf6e6dd0b57d450ea75af45c267e
Marco "Mint" Introini <mint.dev@pm.me>
2024-02-18 15:16:39 +0100

    Refactor 'invia-email' route and update variable naming
    The 'invia-email' route has been refactored for improved readability and maintenance. This includes renaming the 'testoHtml' variable to 'htmlText'. Additionally, the Mail facade is now directly imported using the 'use' statement for cleaner code. This update enhances code consistency and readability.

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::post('invia-email', function (){
-    $testoHtml = request()->post('testo');
+Route::post('invia-email', function () {
+    $htmlText = request()->post('testo');
 
-    \Illuminate\Support\Facades\Mail::html($testoHtml, function ($message){
+    \Illuminate\Support\Facades\Mail::html($htmlText, function ($message) {
         $message->from('noreply@example.com', 'Example');
         $message->to('john@example.com');
         $message->subject('Esempio Invio email');
